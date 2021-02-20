4bfb65109df2d124b0c70dbe11115130ff05bafa
Arpit Dubey <arpit@gamezop.co>
2021-02-20 21:37:23 +0000

    final changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<div>hello rseasct</div>, document.getElementById("app"));
+render(<div>heallo rseasct</div>, document.getElementById("app"));
